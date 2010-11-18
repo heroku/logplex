bee0e6ea6f8c5fe3816ebbd9a2ade883c5895a0e
Jacob Vorreuter <jake@heroku.com>
2010-11-17 18:07:12 -0800

    direct output to log

--- bin/logplex.sh
+++ bin/logplex.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
-erl +K true +A30 -name logplex_console@`hostname --fqdn` -pa ebin -pa deps/*/ebin -noshell -detached -boot release/logplex-1.0
+export USER=logplex
+export DIR=/opt/logplex
+export SERVER_UID=`id -u $USER`
+export SERVER_GID=`id -g $USER`
+
+cd $DIR
+erl +K true +A30 -name logplex_console@`hostname --fqdn` -pa ebin -pa deps/*/ebin -noshell -boot release/logplex-1.0 > /var/log/$USER.log &2>1
