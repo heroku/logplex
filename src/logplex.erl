4172ce83e608a3bfa81e9cadc345db9c8768ab78
Jacob Vorreuter <jake@heroku.com>
2010-11-23 12:32:05 -0800

    add throughput notification to logs

--- src/logplex.erl
+++ src/logplex.erl
@@ -9,20 +9,35 @@ route(Token, Msg) ->
             Count = logplex_stats:incr(ChannelId),
             case exceeded_threshold(Count, Addon) of
                 true ->
-                    io:format("channel ~p has exceeded its limit: ~w~n", [ChannelId, Count]);
+                    ok;
+                notify ->
+                    {{Year,Month,Day},{Hour,Min,Sec}} = Local = erlang:localtime(),
+                    UTC = erlang:universaltime(),
+                    {_, {Offset, _, _}} = calendar:time_difference(Local, UTC),
+                    iolist_to_binary(io_lib:format("<40>1 ~w-~w-~wT~w:~w:~w-0~w:00 - heroku logplex - - You have exceeded ~w logs/min. Please upgrade your logging addon for higher throughput.", [Year, Month, Day, Hour, Min, Sec, Offset, throughput(Addon)])),
+                    process(ChannelId, Addon, Msg);
                 false ->
                     Msg1 = re:replace(Msg, Token, TokenName),
                     Msg2 = iolist_to_binary(Msg1),
-                    logplex_channel:push(ChannelId, Addon, Msg2),
-                    logplex_tail:route(ChannelId, Msg2),
-                    [logplex_drain_pool:route(Host, Port, Msg2) || [_Channel, {host, Host}, {port, Port}] <- logplex_channel:drains(ChannelId)],
+                    process(ChannelId, Addon, Msg2),
                     logplex_stats:incr(message_processed)
             end;
         _ ->
             ok
     end.
 
+process(ChannelId, Addon, Msg) ->
+    logplex_channel:push(ChannelId, Addon, Msg),
+    logplex_tail:route(ChannelId, Msg),
+    [logplex_drain_pool:route(Host, Port, Msg) || [_Channel, {host, Host}, {port, Port}] <- logplex_channel:drains(ChannelId)],
+    ok.
+
+throughput(<<"basic">>) -> ?BASIC_THROUGHPUT;
+throughput(<<"expanded">>) -> ?EXPANDED_THROUGHPUT.
+
 exceeded_threshold(_, <<"advanced">>) -> false;
-exceeded_threshold(Count, <<"expanded">>) when Count =< 10000 -> false;
-exceeded_threshold(Count, <<"basic">>) when Count =< 500 -> false;
+exceeded_threshold(Count, <<"expanded">>) when Count =< ?EXPANDED_THROUGHPUT -> false;
+exceeded_threshold(Count, <<"expanded">>) when Count == (?EXPANDED_THROUGHPUT + 1) -> notify;
+exceeded_threshold(Count, <<"basic">>) when Count =< ?BASIC_THROUGHPUT -> false;
+exceeded_threshold(Count, <<"basic">>) when Count == (?BASIC_THROUGHPUT + 1) -> notify;
 exceeded_threshold(_, _) -> true.
