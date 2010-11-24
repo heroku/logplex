c0443416e57b9668ff44497128f5a25c51629234
Jacob Vorreuter <jake@heroku.com>
2010-11-24 13:40:09 -0800

    fix startup sscript

--- bin/logplex.sh
+++ bin/logplex.sh
@@ -4,7 +4,8 @@ export USER=logplex
 export DIR=/opt/logplex
 export SERVER_UID=`id -u $USER`
 export SERVER_GID=`id -g $USER`
+export HOME=/root
 
 ulimit -n 65535
 cd $DIR
-erl +K true +A30 +P500000 -env ERL_MAX_PORTS 65535 -name logplex@`hostname --fqdn` -pa ebin -pa deps/*/ebin -noshell -boot release/logplex-1.0 > /var/log/$USER.log &2>1
+erl +K true +A30 +P500000 -env ERL_MAX_PORTS 65535 -name logplex@`hostname --fqdn` -pa ebin -pa deps/*/ebin -noshell -boot release/logplex-1.0 > /var/log/$USER.log 2>&1
