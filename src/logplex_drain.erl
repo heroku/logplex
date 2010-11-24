293f0acc2976e6bb4bbf3685dbd4d3936c40d1c8
Jacob Vorreuter <jake@heroku.com>
2010-11-24 11:24:27 -0800

    asdf

--- src/logplex_drain.erl
+++ src/logplex_drain.erl
@@ -13,6 +13,9 @@
 start_link() ->
 	gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 
+create(ChannelId, Host, Port) when is_integer(Port) ->
+    create(ChannelId, Host, list_to_binary(integer_to_list(Port)));
+
 create(ChannelId, Host, Port) when is_binary(ChannelId), is_binary(Host), (is_binary(Port) orelse Port == undefined) ->
     case ets:match_object(?MODULE, #drain{id='_', channel_id=ChannelId, host=Host, port=Port}) of
         [_] ->
