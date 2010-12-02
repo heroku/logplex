068c365a43b2ece63f5a862cb9b107d97e8228d5
Jacob Vorreuter <jake@heroku.com>
2010-12-02 12:07:13 -0800

    pipe to logger

--- bin/logplex.sh
+++ bin/logplex.sh
@@ -8,4 +8,4 @@ export HOME=/root
 
 ulimit -n 65535
 cd $DIR
-erl +K true +A30 +P500000 -env ERL_MAX_PORTS 65535 -name logplex@`hostname --fqdn` -pa ebin -pa deps/*/ebin -noshell -boot release/logplex-1.0 > /var/log/$USER.log 2>&1
+erl +K true +A30 +P500000 -env ERL_MAX_PORTS 65535 -name logplex@`hostname --fqdn` -pa ebin -pa deps/*/ebin -noshell -boot release/logplex-1.0 | logger -p local5.info
