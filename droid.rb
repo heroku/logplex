b334d8b4352490426031b85d2f4fa732dafd8be8
Jacob Vorreuter <jake@heroku.com>
2010-12-09 11:38:00 -0800

    remove droid

--- droid.rb
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env ruby
-require 'rubygems'
-require 'droid/heroku'
-
-HerokuDroid.new('Logplex') do |droid|
-end
--- droid.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export USER=logplex
-export DIR=/opt/logplex
-export SERVER_UID=`id -u $USER`
-export SERVER_GID=`id -g $USER`
-
-cd $DIR
-ruby ./droid.rb > /var/log/droid.log 2>&1
