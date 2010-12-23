6e3d136a7eb139ce9748c76ebd4dcf7762870db9
Jacob Vorreuter <jake@heroku.com>
2010-12-22 18:03:46 -0800

    removing hard coded items in unit test

--- test/logplex_api_tests.erl
+++ test/logplex_api_tests.erl
@@ -2,6 +2,11 @@
 -include_lib("eunit/include/eunit.hrl").
 
 logplex_api_test_() ->
+    Port =
+        case os:getenv("HTTP_PORT") of
+            false -> "80";
+            Val -> Val
+        end,
     AppId = 1,
     AppName = "app1@logplex.heroku.com",
     MsgBodies = [
@@ -15,27 +20,27 @@ logplex_api_test_() ->
     {setup,
         fun inets:start/0,
         [{"Healthcheck", 
-            ?_assertMatch({ok,{{_,200,_},_,"OK"}}, http:request(get, {"http://localhost:8002/healthcheck", headers()}, [], []))},
+            ?_assertMatch({ok,{{_,200,_},_,"OK"}}, http:request(get, {"http://localhost:" ++ Port ++ "/healthcheck", headers()}, [], []))},
          {"Cloudkick",
-            ?_assertMatch({ok,{{_,200,_},_,"{\"state\":\"ok\",\"metrics\":[]}"}}, http:request(get, {"http://localhost:8002/cloudkick", headers()}, [], []))},
+            ?_assertMatch({ok,{{_,200,_},_,"{\"state\":\"ok\",\"metrics\":[]}"}}, http:request(get, {"http://localhost:" ++ Port ++ "/cloudkick", headers()}, [], []))},
          {"Missing channel name for create channel",
             ?_assertMatch({ok,{{_,400,_},_,"'name' post param missing"}},
                 http:request(post, 
-                    {"http://localhost:8002/channels", headers(), content_type(), iolist_to_binary(mochijson2:encode({struct, [
+                    {"http://localhost:" ++ Port ++ "/channels", headers(), content_type(), iolist_to_binary(mochijson2:encode({struct, [
                         {"app_id", AppId},
                         {"addon", <<"expanded">>}
                     ]}))}, [], []))},
         {"Poorly formatted channel name for create channel",
            ?_assertMatch({ok,{{_,400,_},_,"'name' post param missing"}},
                http:request(post, 
-                   {"http://localhost:8002/channels", headers(), content_type(), iolist_to_binary(mochijson2:encode({struct, [
+                   {"http://localhost:" ++ Port ++ "/channels", headers(), content_type(), iolist_to_binary(mochijson2:encode({struct, [
                        {"app_id", AppId},
                        {"addon", <<"expanded">>}
                    ]}))}, [], []))},
          {"Create channel",
             fun() ->
                 Ret = http:request(post, 
-                    {"http://localhost:8002/channels", headers(), content_type(), iolist_to_binary(mochijson2:encode({struct, [
+                    {"http://localhost:" ++ Port ++ "/channels", headers(), content_type(), iolist_to_binary(mochijson2:encode({struct, [
                         {"app_id", AppId},
                         {"addon", <<"expanded">>},
                         {"name", list_to_binary(AppName)}
@@ -47,7 +52,7 @@ logplex_api_test_() ->
          {"Create app token",
             fun() ->
                 Ret = http:request(post, 
-                    {"http://localhost:8002/channels/" ++ get(channel_id) ++ "/token", headers(), content_type(), iolist_to_binary(mochijson2:encode({struct, [
+                    {"http://localhost:" ++ Port ++ "/channels/" ++ get(channel_id) ++ "/token", headers(), content_type(), iolist_to_binary(mochijson2:encode({struct, [
                         {"channel_id", list_to_binary(get(channel_id))},
                         {"name", <<"app">>}
                     ]}))}, [], []),
@@ -58,7 +63,7 @@ logplex_api_test_() ->
          {"Create Heroku token",
             fun() ->
                 Ret = http:request(post, 
-                    {"http://localhost:8002/channels/" ++ get(channel_id) ++ "/token", headers(), content_type(), iolist_to_binary(mochijson2:encode({struct, [
+                    {"http://localhost:" ++ Port ++ "/channels/" ++ get(channel_id) ++ "/token", headers(), content_type(), iolist_to_binary(mochijson2:encode({struct, [
                         {"channel_id", list_to_binary(get(channel_id))},
                         {"name", <<"heroku">>}
                     ]}))}, [], []),
@@ -69,19 +74,19 @@ logplex_api_test_() ->
          {"Upgrade to advanced addon",
             ?_assertMatch({ok,{{_,200,_},_,"OK"}},
                 http:request(post, 
-                    {"http://localhost:8002/channels/" ++ get(channel_id) ++ "/addon", headers(), content_type(), iolist_to_binary(mochijson2:encode({struct, [
+                    {"http://localhost:" ++ Port ++ "/channels/" ++ get(channel_id) ++ "/addon", headers(), content_type(), iolist_to_binary(mochijson2:encode({struct, [
                         {"addon", <<"advanced">>}
                     ]}))}, [], []))},
          {"Upgrade addon for non-existent channel",
            ?_assertMatch({ok,{{_,404,_},_,"Not found"}},
                http:request(post, 
-                   {"http://localhost:8002/channels/0/addon", headers(), content_type(), iolist_to_binary(mochijson2:encode({struct, [
+                   {"http://localhost:" ++ Port ++ "/channels/0/addon", headers(), content_type(), iolist_to_binary(mochijson2:encode({struct, [
                        {"addon", <<"advanced">>}
                    ]}))}, [], []))},
          {"Create session",
             fun() ->
                 Ret = http:request(post, 
-                    {"http://localhost:8002/sessions", headers(), content_type(), iolist_to_binary(mochijson2:encode({struct, [
+                    {"http://localhost:" ++ Port ++ "/sessions", headers(), content_type(), iolist_to_binary(mochijson2:encode({struct, [
                         {"channel_id", list_to_binary(get(channel_id))},
                         {"name", list_to_binary(AppName)}
                     ]}))}, [], []),
@@ -92,27 +97,27 @@ logplex_api_test_() ->
          {"Create drain",
             ?_assertMatch({ok,{{_,201,_},_,"OK"}},
                 http:request(post, 
-                    {"http://localhost:8002/channels/" ++ get(channel_id) ++ "/drains", headers(), content_type(), iolist_to_binary(mochijson2:encode({struct, [
+                    {"http://localhost:" ++ Port ++ "/channels/" ++ get(channel_id) ++ "/drains", headers(), content_type(), iolist_to_binary(mochijson2:encode({struct, [
                         {"host", <<"10.0.0.1">>},
                         {"port", 514}
                     ]}))}, [], []))},
          {"Create duplicate drain",
             ?_assertMatch({ok,{{_,400,_},_,"Drain already exists"}},
                 http:request(post, 
-                    {"http://localhost:8002/channels/" ++ get(channel_id) ++ "/drains", headers(), content_type(), iolist_to_binary(mochijson2:encode({struct, [
+                    {"http://localhost:" ++ Port ++ "/channels/" ++ get(channel_id) ++ "/drains", headers(), content_type(), iolist_to_binary(mochijson2:encode({struct, [
                         {"host", <<"10.0.0.1">>},
                         {"port", 514}
                     ]}))}, [], []))},
          {"Create invalid drain",
            ?_assertMatch({ok,{{_,400,_},_,"Invalid drain"}},
                http:request(post, 
-                   {"http://localhost:8002/channels/" ++ get(channel_id) ++ "/drains", headers(), content_type(), iolist_to_binary(mochijson2:encode({struct, [
+                   {"http://localhost:" ++ Port ++ "/channels/" ++ get(channel_id) ++ "/drains", headers(), content_type(), iolist_to_binary(mochijson2:encode({struct, [
                        {"host", <<"donkey">>},
                        {"port", 514}
                    ]}))}, [], []))},
          {"Get info",
             fun() ->
-                Ret = http:request(get, {"http://localhost:8002/channels/" ++ get(channel_id) ++ "/info", headers()}, [], []),
+                Ret = http:request(get, {"http://localhost:" ++ Port ++ "/channels/" ++ get(channel_id) ++ "/info", headers()}, [], []),
                 InfoStr =
                 "{\"channel_id\":" ++ get(channel_id) ++ "," ++
                  "\"channel_name\":\"" ++ AppName ++ "\"," ++
@@ -124,7 +129,7 @@ logplex_api_test_() ->
             end},
          {"Get drain list",
             ?_assertMatch({ok,{{_,200,_},_,"[{\"host\":\"10.0.0.1\",\"port\":514}]"}},
-                http:request(get, {"http://localhost:8002/channels/" ++ get(channel_id) ++ "/drains", headers()}, [], []))},
+                http:request(get, {"http://localhost:" ++ Port ++ "/channels/" ++ get(channel_id) ++ "/drains", headers()}, [], []))},
          {"Input logs",
             fun() ->
                 [begin
@@ -136,7 +141,7 @@ logplex_api_test_() ->
             end},
          {"Verify logs",
             fun() ->
-                Ret = http:request(get, {"http://localhost:8002" ++ get(session), headers()}, [], []),
+                Ret = http:request(get, {"http://localhost:" ++ Port ++ get(session), headers()}, [], []),
                 ?assertMatch({ok,{{_,200,_},_,_}}, Ret),
                 {ok,{{_,200,_},_,Logs}} = Ret,
                 Logs1 = string:tokens(Logs, "\n"),
@@ -152,7 +157,7 @@ logplex_api_test_() ->
          {"Create session",
             fun() ->
                 Ret = http:request(post, 
-                    {"http://localhost:8002/sessions", headers(), content_type(), iolist_to_binary(mochijson2:encode({struct, [
+                    {"http://localhost:" ++ Port ++ "/sessions", headers(), content_type(), iolist_to_binary(mochijson2:encode({struct, [
                         {"channel_id", list_to_binary(get(channel_id))},
                         {"name", list_to_binary(AppName)},
                         {"source", <<"app">>}
@@ -163,7 +168,7 @@ logplex_api_test_() ->
             end},
          {"Verify source filter",
             fun() ->
-                Ret = http:request(get, {"http://localhost:8002" ++ get(session), headers()}, [], []),
+                Ret = http:request(get, {"http://localhost:" ++ Port ++ get(session), headers()}, [], []),
                 ?assertMatch({ok,{{_,200,_},_,_}}, Ret),
                 {ok,{{_,200,_},_,Logs}} = Ret,
                 Logs1 = string:tokens(Logs, "\n"),
@@ -179,7 +184,7 @@ logplex_api_test_() ->
          {"Create session",
             fun() ->
                 Ret = http:request(post, 
-                    {"http://localhost:8002/sessions", headers(), content_type(), iolist_to_binary(mochijson2:encode({struct, [
+                    {"http://localhost:" ++ Port ++ "/sessions", headers(), content_type(), iolist_to_binary(mochijson2:encode({struct, [
                         {"channel_id", list_to_binary(get(channel_id))},
                         {"name", list_to_binary(AppName)},
                         {"ps", <<"router">>}
@@ -190,7 +195,7 @@ logplex_api_test_() ->
             end},
          {"Verify ps filter",
             fun() ->
-                Ret = http:request(get, {"http://localhost:8002" ++ get(session), headers()}, [], []),
+                Ret = http:request(get, {"http://localhost:" ++ Port ++ get(session), headers()}, [], []),
                 ?assertMatch({ok,{{_,200,_},_,_}}, Ret),
                 {ok,{{_,200,_},_,Logs}} = Ret,
                 Logs1 = string:tokens(Logs, "\n"),
@@ -206,13 +211,13 @@ logplex_api_test_() ->
          {"Downgrade to basic addon",
             ?_assertMatch({ok,{{_,200,_},_,"OK"}},
                 http:request(post, 
-                    {"http://localhost:8002/channels/" ++ get(channel_id) ++ "/addon", headers(), content_type(), iolist_to_binary(mochijson2:encode({struct, [
+                    {"http://localhost:" ++ Port ++ "/channels/" ++ get(channel_id) ++ "/addon", headers(), content_type(), iolist_to_binary(mochijson2:encode({struct, [
                         {"addon", <<"basic">>}
                     ]}))}, [], []))},
          {"Create session",
            fun() ->
                Ret = http:request(post, 
-                   {"http://localhost:8002/sessions", headers(), content_type(), iolist_to_binary(mochijson2:encode({struct, [
+                   {"http://localhost:" ++ Port ++ "/sessions", headers(), content_type(), iolist_to_binary(mochijson2:encode({struct, [
                        {"channel_id", list_to_binary(get(channel_id))},
                        {"name", list_to_binary(AppName)}
                    ]}))}, [], []),
@@ -226,7 +231,7 @@ logplex_api_test_() ->
                 Msg = iolist_to_binary(["<40>1 ", logplex_utils:formatted_utc_date(), " ", atom_to_list(node()), " ", get(SourceKey), " ", Ps, " - - ", Body]),
                 [gen_udp:send(UdpSock, "127.0.0.1", 9999, Msg) || _ <- lists:seq(1, 501)],
                 timer:sleep(100),
-                Ret = http:request(get, {"http://localhost:8002" ++ get(session), headers()}, [], []),
+                Ret = http:request(get, {"http://localhost:" ++ Port ++ get(session), headers()}, [], []),
                 ?assertMatch({ok,{{_,200,_},_,_}}, Ret),
                 {ok,{{_,200,_},_,Logs}} = Ret,
                 Logs1 = string:tokens(Logs, "\n"),
@@ -240,20 +245,20 @@ logplex_api_test_() ->
             end},
          {"Delete drain",
             ?_assertMatch({ok,{{_,200,_},_,_}},
-                http:request(delete, {"http://localhost:8002/channels/" ++ get(channel_id) ++ "/drains?host=10.0.0.1&port=514", headers()}, [], []))},
+                http:request(delete, {"http://localhost:" ++ Port ++ "/channels/" ++ get(channel_id) ++ "/drains?host=10.0.0.1&port=514", headers()}, [], []))},
          {"Delete non-existent drain",
             ?_assertMatch({ok,{{_,404,_},_,"Not found"}},
-                http:request(delete, {"http://localhost:8002/channels/" ++ get(channel_id) ++ "/drains?host=10.0.0.1&port=514", headers()}, [], []))},
+                http:request(delete, {"http://localhost:" ++ Port ++ "/channels/" ++ get(channel_id) ++ "/drains?host=10.0.0.1&port=514", headers()}, [], []))},
          {"Delete channel",
             ?_assertMatch({ok,{{_,200,_},_,_}},
-                http:request(delete, {"http://localhost:8002/channels/" ++ get(channel_id), headers()}, [], []))},
+                http:request(delete, {"http://localhost:" ++ Port ++ "/channels/" ++ get(channel_id), headers()}, [], []))},
          {"Delete non-existent channel",
             ?_assertMatch({ok,{{_,404,_},_,"Not found"}},
-               http:request(delete, {"http://localhost:8002/channels/" ++ get(channel_id), headers()}, [], []))},
+               http:request(delete, {"http://localhost:" ++ Port ++ "/channels/" ++ get(channel_id), headers()}, [], []))},
          {"Verify healthcheck 'Zero child processes running'",
             fun() ->
                 [logplex_queue:stop(Pid) || {_,Pid,_,_} <- supervisor:which_children(logplex_read_queue_sup)],
-                ?assertMatch({ok,{{_,500,_},_,"Zero logplex_read_queue_sup child processes running"}}, http:request(get, {"http://localhost:8002/healthcheck", headers()}, [], []))
+                ?assertMatch({ok,{{_,500,_},_,"Zero logplex_read_queue_sup child processes running"}}, http:request(get, {"http://localhost:" ++ Port ++ "/healthcheck", headers()}, [], []))
             end}
         ]
     }.
@@ -262,7 +267,7 @@ content_type() ->
     "application/json".
 
 headers() ->
-    [{"Authorization", "secret"}, {"Content-Type", content_type()}].
+    [{"Authorization", os:getenv("LOGPLEX_AUTH_KEY")}, {"Content-Type", content_type()}].
 
 source_key_to_source(app_token) -> "app";
 source_key_to_source(heroku_token) -> "heroku".
