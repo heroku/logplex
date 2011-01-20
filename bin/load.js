87cd23fa3b0b529a5700c804c0260f72ebbd8bbf
Jacob Vorreuter <jake@heroku.com>
2011-01-19 18:49:13 -0800

    replacing load.js script

--- bin/load.js
+++ bin/load.js
@@ -1,8 +1,37 @@
 var dgram = require('dgram');
-var message = new Buffer("<40>1 2010-11-10T17:16:33-08:00 domU-12-31-39-13-74-02 t.8f5f231c-d915-4c46-8b03-c838533b3c25 web.1 - - State changed from created to starting");
-//var message = new Buffer("Nov 29 12:51:31 domU-12-31-39-00-66-52 t.641de56d-098c-441e-b8dc-a3e1789675d6[router]: GET vivid-fire-26.jake.herokudev.com/ dyno=web.1 queue=0 wait=0ms service=1ms bytes=215")
-var client = dgram.createSocket("udp4");
-while(true) {
-	client.send(message, 0, message.length, 9999, "127.0.0.1");
+var sys = require('sys');
+
+var token;
+var host;
+var port;
+var rate = 100;
+
+for (i = 2; i < process.argv.length; i++) {
+  switch (process.argv[i]) {
+    case "--token":
+      token = process.argv[++i];
+      break;
+    case "--host":
+      host = process.argv[++i];
+      break;
+    case "--port":
+      port = parseInt(process.argv[++i]);
+      break;
+    case "--rate":
+      rate = parseInt(process.argv[++i]);
+      break;
+  }
 }
-client.close();
+
+var message = new Buffer("<40>1 2010-11-10T17:16:33-08:00 domU-12-31-39-13-74-02 " + token + " web.1 - - State changed from created to starting");
+
+var client = dgram.createSocket("udp4");
+
+var tick = function(counter) {
+  client.send(message, 0, message.length, port, host);
+  if (counter == rate) setTimeout(tick, 1, 0);
+  else tick(counter+1);
+};
+
+setTimeout(tick, 1, 0);
+
