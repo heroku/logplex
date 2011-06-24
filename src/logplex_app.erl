736d63dc9b23d0f9f4bd770b419ed61560e4e2ff
Jacob Vorreuter <jacob.vorreuter@gmail.com>
2011-06-24 11:26:35 -0700

    update tcp acceptor code

--- src/logplex_app.erl
+++ src/logplex_app.erl
@@ -66,6 +66,8 @@ init([]) ->
         {logplex_work_queue, {logplex_queue, start_link, [logplex_work_queue, logplex_work_queue_args()]}, permanent, 2000, worker, [logplex_work_queue]},
         {logplex_drain_buffer, {logplex_queue, start_link, [logplex_drain_buffer, logplex_drain_buffer_args()]}, permanent, 2000, worker, [logplex_drain_buffer]},
 
+        {tcp_proxy_sup, {tcp_proxy_sup, start_link, []}, permanent, 2000, worker, [tcp_proxy_sup]},
+
         {logplex_api, {logplex_api, start_link, []}, permanent, 2000, worker, [logplex_api]},
         {udp_acceptor, {udp_acceptor, start_link, []}, permanent, 2000, worker, [udp_acceptor]}] ++
         [{tcp_acceptor, {tcp_acceptor, start_link, [?TCP_PORT]}, permanent, 2000, worker, [tcp_acceptor]} || os:getenv("TCP_ACCEPTOR") == "1"]
