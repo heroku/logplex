93f419b7a68992d84a5cd603f363d0e551d8ffc6
Jacob Vorreuter <jacob.vorreuter@gmail.com>
2011-09-19 13:08:22 -0700

    monitor status of drain writer processes

---
+++ src/logplex_drain_writer_mon.erl
@@ -0,0 +1,41 @@
+%% Copyright (c) 2010 Jacob Vorreuter <jacob.vorreuter@gmail.com>
+%% 
+%% Permission is hereby granted, free of charge, to any person
+%% obtaining a copy of this software and associated documentation
+%% files (the "Software"), to deal in the Software without
+%% restriction, including without limitation the rights to use,
+%% copy, modify, merge, publish, distribute, sublicense, and/or sell
+%% copies of the Software, and to permit persons to whom the
+%% Software is furnished to do so, subject to the following
+%% conditions:
+%% 
+%% The above copyright notice and this permission notice shall be
+%% included in all copies or substantial portions of the Software.
+%% 
+%% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+%% EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+%% OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+%% NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+%% HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+%% WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+%% FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+%% OTHER DEALINGS IN THE SOFTWARE.
+-module(logplex_drain_writer_mon).
+-export([start_link/0, init/1, loop/0]).
+
+start_link() ->
+    proc_lib:start_link(?MODULE, init, [self()]).
+
+init(Parent) ->
+    proc_lib:init_ack(Parent, {ok, self()}),
+    loop().
+
+loop() ->
+    [case proplists:get_value(status, process_info(Pid)) of
+        suspended ->
+            io:format("~p event=kill pid=~p~n", [?MODULE, Pid]),
+            exit(Pid, kill);
+        _Other -> ok
+    end || {_, Pid, _, _} <- supervisor:which_children(logplex_drain_sup)],
+    timer:sleep(10 * 1000),
+    ?MODULE:loop().
