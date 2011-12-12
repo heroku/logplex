e70183b4ed3c104b006b5329dc6d96c7b48c230e
Geoff Cant <nem@erlang.geek.nz>
2011-12-12 14:16:40 -0800

    Queue messages from tcp now, but not udp.

--- src/tcp_proxy.erl
+++ src/tcp_proxy.erl
@@ -99,7 +99,7 @@ parse(Accept, RE, Packet) ->
             Size = list_to_integer(binary_to_list(Len)),
             case Packet of
                 <<Len:LSize/binary, 32/integer, Msg:Size/binary, Rest1/binary>> ->
-                    process_msg(),
+                    process_msg(Msg),
                     parse(Accept, RE, Rest1);
                 _ ->
                     {ok, Packet}
@@ -115,7 +115,8 @@ read_line(<<>>, Acc) ->
 read_line(<<C, Rest/binary>>, Acc) ->
     read_line(Rest, [C|Acc]).
 
-process_msg() ->
+process_msg(Msg) ->
     logplex_stats:incr(message_received_tcp),
-    logplex_realtime:incr(message_received_tcp).
+    logplex_realtime:incr(message_received_tcp),
+    logplex_queue:in(logplex_work_queue, Msg).
 
