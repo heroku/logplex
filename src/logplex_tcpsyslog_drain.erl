df4dd65a90331058ae664412cef0712ab2398e52
Geoff Cant <nem@erlang.geek.nz>
2012-01-18 16:17:00 -0800

    Update record types to use drain/channel types.

--- src/logplex_tcpsyslog_drain.erl
+++ src/logplex_tcpsyslog_drain.erl
@@ -21,8 +21,8 @@
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
          terminate/2, code_change/3]).
 
--record(state, {id :: binary(),
-                channel :: binary(),
+-record(state, {id :: logplex_drain:id(),
+                channel :: logplex_channel:id(),
                 host :: string() | inet:ip_address(),
                 port :: inet:port_number(),
                 sock = undefined :: 'undefined' | inet:socket(),
