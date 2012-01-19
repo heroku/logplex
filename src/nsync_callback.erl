84ac29e00f6145b10f780aee07ea5b6028eaf139
Geoff Cant <nem@erlang.geek.nz>
2012-01-19 14:34:10 -0800

    Alter nsync to follow drain:stop/2->1 api change.

--- src/nsync_callback.erl
+++ src/nsync_callback.erl
@@ -86,7 +86,7 @@ handle({cmd, "del", [<<"tok:", Rest/binary>> | _Args]}) ->
 handle({cmd, "del", [<<"drain:", Rest/binary>> | _Args]}) ->
     Id = list_to_integer(parse_id(Rest)),
     ?INFO("at=delete type=drain id=~p", [Id]),
-    logplex_drain:stop(tcpsyslog, Id),
+    logplex_drain:stop(Id),
     remove_token_drain_data(Id),
     ets:delete(drains, Id);
 
