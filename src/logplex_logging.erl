addb88bf1345dbedf32fa59e3c52d99100f90f5d
Geoff Cant <nem@erlang.geek.nz>
2012-01-25 14:44:01 -0800

    Type info for logplex_logging functions.

--- src/logplex_logging.erl
+++ src/logplex_logging.erl
@@ -1,15 +1,19 @@
 %% @copyright Geoff Cant
 %% @author Geoff Cant <nem@erlang.geek.nz>
 %% @version {@vsn}, {@date} {@time}
-%% @doc 
+%% @doc logging utility and formatting functions
 %% @end
 -module(logplex_logging).
 
 -export([dest/2]).
 
+-type host() :: inet:ip4_address() | iolist() | binary().
+
+-spec dest(host(), inet:port_number()) -> binary().
 dest(Host, Port) ->
     iolist_to_binary([host_str(Host), ":", integer_to_list(Port)]).
 
+-spec host_str(host()) -> iolist() | binary().
 host_str({A,B,C,D}) ->
     Quads = [integer_to_list(Quad) || Quad <- [A,B,C,D]],
     string:join(Quads,".");
