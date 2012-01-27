08f0af3759f869065aaf96d53c363a17b9369c06
Geoff Cant <nem@erlang.geek.nz>
2012-01-27 15:29:22 -0800

    Adds the v32->v33 upgrade shellcode.

---
+++ upgrades/v32_v33/live_upgrade.erl
@@ -0,0 +1,44 @@
+f(ChildPids).
+ChildPids = fun (Sup) ->
+              [ element(2, R) || R <- supervisor:which_children(Sup) ]
+            end.
+
+f(UpgradeNode).
+UpgradeNode = fun () ->
+  io:format(whereis(user), "at=upgrade_start~n", []),
+  %% New modules
+  l(logplex_shard_info),
+  l(logplex_logging),
+
+  %% Logging changes
+  l(logplex_api),
+  l(logplex_drain),
+  l(logplex_app),
+  l(logplex_queue),
+  l(logplex_realtime),
+  l(logplex_redis_writer),
+  l(logplex_utils),
+
+  l(logplex_stats),
+  l(logplex_tcpsyslog_drain),
+  
+  %% logplex_shard_info stuff
+  l(logplex_channel),
+  l(loglex_worker),
+
+  Pid = whereis(logplex_shard),
+
+  sys:suspend(Pid),
+  l(logplex_shard),
+  sys:change_code(Pid, logplex_shard, v32, undefined),
+  sys:resume(logplex_shard),
+
+  Proxies = ChildPids(tcp_proxy_sup),
+  lists:map(fun sys:suspend/1, Proxies),
+  l(tcp_proxy),
+  [ sys:change_code(P, tcp_proxy, v32, undefined) || P <- Proxies ],
+  lists:map(fun sys:resume/1, Proxies),
+
+  io:format(whereis(user), "at=upgrade_end~n", []),
+  gen_server:call(Pid, consistency_check)
+end.
---
+++ upgrades/v32_v33/rolling_upgrade.erl
@@ -0,0 +1,14 @@
+f(RollingUpgrade).
+RollingUpgrade = fun (Nodes) ->
+  lists:foldl(fun (N, {good, Upgraded}) ->
+    case rpc:call(N, erlang, apply, [ UpgradeNode, [] ]) of
+      consistent ->
+        {good, [N | Upgraded]};
+      Else ->
+        {{bad, N, Else}, Upgraded}
+    end;
+    (N, {_, _} = Acc) -> Acc
+    end,
+    {good, []},
+    Nodes)
+end.
