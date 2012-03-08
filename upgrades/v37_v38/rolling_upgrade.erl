644054599d6632c288225f3a0246abb8e6332762
Geoff Cant <nem@erlang.geek.nz>
2012-03-08 14:50:38 -0800

    Moar upgrade functionality.

--- upgrades/v37_v38/rolling_upgrade.erl
+++ upgrades/v37_v38/rolling_upgrade.erl
@@ -1,3 +1,18 @@
+f(NodeVersions).
+NodeVersions = fun () ->
+                       F = fun () ->
+                                   {node(), logplex_app:config(git_branch),
+                                    os:getenv("INSTANCE_NAME")}
+                           end,
+                       {Good, _} = rpc:multicall(erlang, apply, [F, [] ]),
+                       lists:keysort(2, Good)
+               end.
+
+f(NodesAt).
+NodesAt = fun (Vsn) ->
+                  [ N || {N, V, _} <- NodeVersions(), V =:= Vsn ]
+          end.
+
 f(RollingUpgrade).
 RollingUpgrade = fun (Nodes) ->
   lists:foldl(fun (N, {good, Upgraded}) ->
@@ -12,3 +27,4 @@ RollingUpgrade = fun (Nodes) ->
     {good, []},
     Nodes)
 end.
+
