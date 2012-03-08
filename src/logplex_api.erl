f795f16f5dfafcd00143a2181faa2eee6283ee49
Geoff Cant <nem@erlang.geek.nz>
2012-03-07 17:58:17 -0800

    removes logplex_stats:workers No longer necessary as we don't use supervisors for those things.

--- src/logplex_api.erl
+++ src/logplex_api.erl
@@ -83,8 +83,6 @@ handlers() ->
     [{['GET', "/healthcheck"], fun(Req, _Match) ->
         authorize(Req),
 
-        [throw({500, io_lib:format("Zero ~p child processes running", [Worker])}) || {Worker, 0} <- logplex_stats:workers()],
-
         RegisteredMods = [logplex_realtime, logplex_stats, logplex_tail, logplex_shard, tcp_acceptor],
         [(whereis(Name) == undefined orelse not is_process_alive(whereis(Name))) andalso throw({500, io_lib:format("Process dead: ~p", [Name])}) || Name <- RegisteredMods],
 
