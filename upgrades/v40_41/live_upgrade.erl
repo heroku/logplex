3e3c0bc3f205bc07d35bc522e06aa4643801d9c0
Geoff Cant <nem@erlang.geek.nz>
2012-04-18 11:20:27 -0400

    Fixing version numbers in the upgrade script.

--- upgrades/v40_41/live_upgrade.erl
+++ upgrades/v40_41/live_upgrade.erl
@@ -2,10 +2,10 @@ f(UpgradeNode).
 UpgradeNode = fun () ->
   case logplex_app:config(git_branch) of
       "v40" ->
-          io:format(whereis(user), "at=upgrade_start cur_vsn=39~n", []);
+          io:format(whereis(user), "at=upgrade_start cur_vsn=40~n", []);
       "v41" ->
           io:format(whereis(user),
-                    "at=upgrade type=retry cur_vsn=40 old_vsn=39~n", []);
+                    "at=upgrade type=retry cur_vsn=41 old_vsn=40~n", []);
       Else ->
           io:format(whereis(user),
                     "at=upgrade_start old_vsn=~p abort=wrong_version", [tl(Else)]),
