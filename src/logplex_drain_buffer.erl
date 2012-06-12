cd1835c6a7dc769efef13018f1fd8f4856ec2864
Geoff Cant <nem@erlang.geek.nz>
2012-06-11 17:34:07 -0700

    Implement a notify/active/passive drain buffer manager.

--- src/logplex_drain_buffer.erl
+++ src/logplex_drain_buffer.erl
@@ -16,13 +16,17 @@
 -record(state, {buf = logplex_msg_buffer:new() :: logplex_msg_buffer:buf(),
                 channel_id :: logplex_channel:id(),
                 owner :: pid(),
-                active_fun :: 'undefined' | logplex_msg_buffer:framing_fun()
+                on_activation :: 'undefined' |
+                                 {TargBytes::pos_integer(),
+                                  logplex_msg_buffer:framing_fun()}
                }).
 
--type mode() :: 'passive' | 'active'.
+%% -type mode() :: 'passive' | 'active' | 'notify'.
 
 -type rx_msgs() :: {'post', Msg::term()}.
--type tx_msgs() :: {'logplex_drain_data', pid(), Data::term()}.
+-type tx_msgs() :: {'logplex_drain_buffer', pid(), 'new_data'} |
+                   {'logplex_drain_buffer', pid(),
+                    {frame, Frame::iolist(), Count::non_neg_integer()}}.
 
 -export_type([rx_msgs/0, tx_msgs/0]).
 
@@ -32,12 +36,18 @@
 
 -export([start_link/2
          ,start_link/1
-         ,start_link/4
-         ,set_active/2
+         ,start_link/3
+         ,set_active/3
+         ,notify/1
         ]).
 
 -export([active/2,
-         passive/2]).
+         passive/2,
+         notify/2
+        ]).
+
+-export([post/2
+        ]).
 
 %% ------------------------------------------------------------------
 %% gen_fsm Function Exports
@@ -55,47 +65,109 @@ start_link(ChannelId) ->
     start_link(ChannelId, self()).
 
 start_link(ChannelId, Owner) ->
-    gen_fsm:start_link(?MODULE, {passive,
-                                 #state{channel_id = ChannelId,
-                                        owner = Owner}}, []).
+    start_link(ChannelId, Owner, notify).
 
--spec start_link(logplex_channel:id(),
-                 pid(),
-                 mode(),
-                 logplex_msg_buffer:framing_fun()) ->
-                        {ok, pid()} | {error, term()} | ignore.
-start_link(ChannelId, Owner, Active, Fun)
-  when Active =:= active orelse Active =:= passive,
-       is_function(Fun),
-       is_pid(Owner) ->
-    gen_fsm:start_link(?MODULE, {Active,
+-spec start_link(ChannelId::logplex_channel:id(),
+                 Owner::pid(),
+                 {active, TargBytes::pos_integer(),
+                  Fun::logplex_msg_buffer:framing_fun()} |
+                 'passive' | 'notify') -> any().
+start_link(ChannelId, Owner, {active, TargBytes, Fun})
+  when is_integer(TargBytes), TargBytes > 0,
+       is_function(Fun, 1) ->
+    gen_fsm:start_link(?MODULE, {active,
                                  #state{channel_id = ChannelId,
                                         owner = Owner,
-                                        active_fun = Fun}}, []).
+                                        on_activation = {TargBytes, Fun}}}, []);
+start_link(ChannelId, Owner, Mode)
+  when Mode =:= passive; Mode =:= notify ->
+    gen_fsm:start_link(?MODULE, {Mode,
+                                 #state{channel_id = ChannelId,
+                                        owner = Owner,
+                                        on_activation = undefined}}, []).
 
--spec set_active(pid() | atom(), logplex_msg_buffer:framing_fun()) -> any().
-set_active(Buffer, Fun) when is_function(Fun, 1) ->
-    Buffer ! {active, Fun}.
+
+
+notify(Buffer) ->
+    gen_fsm:send_event(Buffer, notify).
+
+set_active(Buffer, TargBytes, Fun)
+  when is_integer(TargBytes), TargBytes > 0,
+       is_function(Fun, 1) ->
+    gen_fsm:send_event(Buffer, {set_active, TargBytes, Fun}).
+
+%% ------------------------------------------------------------------
+%% Hand crafted API for humans.
+%% ------------------------------------------------------------------
+
+post(Buffer, Msg) ->
+    Buffer ! {post, Msg}.
 
 %% ------------------------------------------------------------------
 %% gen_fsm Function Definitions
 %% ------------------------------------------------------------------
 
-active(Msg, S = #state{}) ->
-    ?WARN("state=~p Unexpected msg ~p", [active, Msg]),
-    {next_state, active, S}.
-
-passive(Msg, S = #state{}) ->
-    ?WARN("state=~p Unexpected msg ~p", [passive, Msg]),
-    {next_state, passive, S}.
-
 %% @private
 init({Mode, S = #state{channel_id = ChannelId,
                        owner = Owner}})
-  when Mode =:= active orelse Mode =:= passive,
+  when Mode =:= notify orelse Mode =:= passive,
        is_pid(Owner), is_integer(ChannelId) ->
     logplex_channel:register({channel, ChannelId}),
     {ok, Mode, S}.
+
+
+%% @private
+active({set_active, TargBytes, Fun},
+       S = #state{})
+  when is_integer(TargBytes), TargBytes > 0,
+       is_function(Fun, 1) ->
+    {next_state, active, S#state{on_activation={TargBytes, Fun}}};
+active(notify, S = #state{}) ->
+    ?WARN("state=active error=duplicate_activation", []),
+    {next_state, notify, S#state{on_activation=undefined}};
+active(Msg, S = #state{}) ->
+    ?WARN("state=active Unexpected msg ~p", [Msg]),
+    {next_state, active, S}.
+
+
+%% @private
+passive(notify, S = #state{buf = Buf}) ->
+    NewState = S#state{on_activation=undefined},
+    case logplex_msg_buffer:empty(Buf) of
+        empty ->
+            {next_state, notify, NewState};
+        not_empty ->
+            send_notification(NewState)
+    end;
+passive({set_active, TargBytes, Fun}, S = #state{buf = Buf})
+  when is_integer(TargBytes), TargBytes > 0,
+       is_function(Fun, 1) ->
+    NewState = S#state{on_activation={TargBytes, Fun}},
+    case logplex_msg_buffer:empty(Buf) of
+        empty ->
+            {next_state, active, NewState};
+        not_empty ->
+            send(NewState)
+    end;
+passive(Msg, S = #state{}) ->
+    ?WARN("state=passive Unexpected msg ~p", [Msg]),
+    {next_state, passive, S}.
+
+
+%% @private
+notify({set_active, TargBytes, Fun},
+       S = #state{})
+  when is_integer(TargBytes), TargBytes > 0,
+       is_function(Fun, 1) ->
+    {next_state, active, S#state{on_activation={TargBytes, Fun}}};
+notify(notify, S = #state{}) ->
+    ?WARN("state=notify error=duplicate_notification", []),
+    {next_state, notify, S};
+notify(Msg, S = #state{}) ->
+    ?WARN("state=notify Unexpected msg ~p", [Msg]),
+    {next_state, active, S}.
+
+
 
 %% @private
 handle_event(_Event, StateName, State) ->
@@ -115,16 +187,10 @@ handle_info({post, Msg}, StateName, S = #state{buf = OldBuf}) ->
         passive ->
             {next_state, passive, NewState};
         active ->
-            send(NewState)
+            send(NewState);
+        notify ->
+            send_notification(NewState)
     end;
-
-handle_info({active, _}, active, S = #state{}) ->
-    %% XXX - duplicate active once - is ignoring this the right thing
-    %% to do? Otherwise report an error to parent?
-    {next_state, active, S};
-
-handle_info({active, Fun}, passive, S = #state{}) ->
-    become_active(S#state{active_fun=Fun});
 
 handle_info(Info, StateName, State) ->
     ?WARN("state=~p Unexpected info ~p", [StateName, Info]),
@@ -142,17 +208,15 @@ code_change(_OldVsn, StateName, State, _Extra) ->
 %% Internal Function Definitions
 %% ------------------------------------------------------------------
 
-become_active(S = #state{buf = Buf}) ->
-    case logplex_msg_buffer:empty(Buf) of
-        empty ->
-            {next_state, active, S};
-        not_empty ->
-            send(S)
-    end.
+%% @private
+send_notification(S = #state{owner = Owner}) ->
+    Owner ! {logplex_drain_buffer, self(), new_data},
+    {next_state, passive, S}.
 
+%% @private
 send(S = #state{owner = Owner, buf = Buf,
-                active_fun = Fun}) ->
-    {Data, _Count, NewBuf} = logplex_msg_buffer:to_pkts(Buf, 4096, Fun),
-    Owner ! {logplex_drain_data, self(), Data},
-    {next_state, passive,
-     S#state{buf=NewBuf}}.
+                on_activation = {Targ, Fun}}) ->
+    {Frame, Count, NewBuf} = logplex_msg_buffer:to_pkts(Buf, Targ, Fun),
+    NewState = S#state{buf=NewBuf, on_activation=undefined},
+    Owner ! {logplex_drain_buffer, self(), {frame, Frame, Count}},
+    {next_state, passive, NewState}.
