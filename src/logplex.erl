6b9b13b159b51572bec782038d44970ad0a04b26
Geoff Cant <nem@erlang.geek.nz>
2012-10-29 19:42:33 -0700

    Creates the logplex Public API module.
    Can save/restore channels currently.

---
+++ src/logplex.erl
@@ -0,0 +1,26 @@
+%% @copyright Geoff Cant
+%% @author Geoff Cant <nem@erlang.geek.nz>
+%% @version {@vsn}, {@date} {@time}
+%% @doc Logplex Shell functions
+%% @end
+-module(logplex).
+
+-include("logplex_channel.hrl").
+
+-export([serialize_channel/1
+         ,deserialize_channel/1
+        ]).
+
+serialize_channel(ChannelId) when is_integer(ChannelId) ->
+    {logplex_channel:lookup(ChannelId),
+     logplex_token:lookup_by_channel(ChannelId),
+     logplex_drain:lookup_by_channel(ChannelId)}.
+
+deserialize_channel({Chan,
+                     Tokens,
+                     Drains}) ->
+    {logplex_channel:store(Chan),
+     [ logplex_token:store(Token)
+       || Token <- Tokens ],
+     [ logplex_drain:store(Drain)
+       || Drain <- Drains ]}.
