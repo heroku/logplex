0c0e20d718b63d64794561843ee48dc09288e598
Fred Hebert <mononcqc@ferd.ca>
2013-05-01 16:13:30 -0400

    Increasing kill time on health monitor
    The health monitor's own supervisor gives 8 seconds and it's useless to
    give only 2 to its parent. The 8 seconds might be required because
    scanning entire tables for orphans might be a bit long before seeing
    sys messages.

--- src/logplex_sup.erl
+++ src/logplex_sup.erl
@@ -77,7 +77,7 @@ init([]) ->
          permanent, 2000, worker, [logplex_shard]}
 
        ,{logplex_monitor, {logplex_mon_sup, start_link, []},
-         permanent, 2000, supervisor, [logplex_mon_sup]}
+         permanent, 8000, supervisor, [logplex_mon_sup]}
 
        %% All tcp listen processes start from the 'listen' start phase
        %% in logplex_app
