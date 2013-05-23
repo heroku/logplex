cd2b833777e584b7f238eb446f9eece28052c869
Fred Hebert <mononcqc@ferd.ca>
2013-05-23 15:11:58 -0400

    Fixing copyright line

--- src/logplex_tcpsyslog_drain2.erl
+++ src/logplex_tcpsyslog_drain2.erl
@@ -1,4 +1,4 @@
-%% Copyright (c) 2012 Heroku <nem@erlang.geek.nz>
+%% Copyright (c) 2013 Heroku <mononcqc@ferd.ca>
 %%
 %% Permission is hereby granted, free of charge, to any person
 %% obtaining a copy of this software and associated documentation
