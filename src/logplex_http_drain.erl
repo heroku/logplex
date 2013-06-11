53e9fcdf40a0f5e37cc0547edb0b8c887facc670
Fred Hebert <mononcqc@ferd.ca>
2013-06-11 16:54:33 -0700

    Partial support for cowboy 0.8.5
    Also upgrades the header on bad responses to contain the explanation
    instead of the generic HTTP message

--- src/logplex_http_drain.erl
+++ src/logplex_http_drain.erl
@@ -49,7 +49,7 @@
                }).
 
 -define(CONTENT_TYPE, <<"application/logplex-1">>).
--define(HTTP_VERSION, {1,1}).
+-define(HTTP_VERSION, 'HTTP/1.1').
 -define(RECONNECT_MSG, reconnect).
 -define(CONNECT_TIMEOUT, 1000).
 -define(REQUEST_TIMEOUT, 1000).
