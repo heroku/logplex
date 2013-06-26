064271c6da811befdb418cb0210a47b2a105e14e
Ómar Kjartan Yasin <omarkj@gmail.com>
2013-06-26 11:10:33 -0700

    Added a upgrade script for v69 (shard replacements)

---
+++ upgrades/v68_v69/live_upgrade.erl
@@ -0,0 +1,51 @@
+f(UpgradeNode).
+UpgradeNode = fun () ->
+  case logplex_app:config(git_branch) of
+      "v68" ->
+          io:format(whereis(user), "at=upgrade_start cur_vsn=68~n", []);
+      "v69" ->
+          io:format(whereis(user),
+                    "at=upgrade type=retry cur_vsn=68 old_vsn=69~n", []);
+      Else ->
+          io:format(whereis(user),
+                    "at=upgrade_start old_vsn=~p abort=wrong_version", [tl(Else)]),
+          erlang:error({wrong_version, Else})
+  end,
+
+  %% stateless
+  l(logplex_shard),
+
+  application:set_env(logplex, git_branch, "v69"),
+  ok
+end.
+
+f(NodeVersions).
+NodeVersions = fun () ->
+                       lists:keysort(3,
+                           [ {N,
+                              element(2, rpc:call(N, application, get_env, [logplex, git_branch])),
+                              rpc:call(N, os, getenv, ["INSTANCE_NAME"])}
+                             || N <- [node() | nodes()] ])
+               end.
+
+f(NodesAt).
+NodesAt = fun (Vsn) ->
+                  [ N || {N, V, _} <- NodeVersions(), V =:= Vsn ]
+          end.
+
+
+f(RollingUpgrade).
+RollingUpgrade = fun (Nodes) ->
+  lists:foldl(fun (N, {good, Upgraded}) ->
+    case rpc:call(N, erlang, apply, [ UpgradeNode, [] ]) of
+      ok ->
+        {good, [N | Upgraded]};
+      Else ->
+        {{bad, N, Else}, Upgraded}
+    end;
+    (N, {_, _} = Acc) -> Acc
+    end,
+    {good, []},
+    Nodes)
+end.
+
