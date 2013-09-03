0e0a05a768ced4ce1c1ec177ef3fb55824baace8
Fred Hebert <mononcqc@ferd.ca>
2013-09-03 17:02:47 -0400

    Basic batchio inclusion, replaces lager macros
    batches io:format calls into a buffer process to reduce the number
    of calls required and also do overload protection automatically.
    
    Experimental material to see if it helps with performance, given the
    buffering and load shedding allow to go asynchronous on log messages
    without loosing too much data.

--- include/logplex_logging.hrl
+++ include/logplex_logging.hrl
@@ -7,13 +7,13 @@
 -compile([{parse_transform, lager_transform}]).
 
 -define(INFO(Format, Args),
-        lager:info("pid=~p m=~p ln=~p class=info " ++ Format,
-                   [self(), ?MODULE, ?LINE | Args])).
+        batchio:format("pid=~p m=~p ln=~p class=info " ++ Format ++ "~n",
+                       [self(), ?MODULE, ?LINE | Args])).
 -define(WARN(Format, Args),
-        lager:warning("pid=~p m=~p ln=~p class=warn " ++ Format,
-                      [self(), ?MODULE, ?LINE | Args])).
+        batchio:format("pid=~p m=~p ln=~p class=warn " ++ Format ++ "~n",
+                       [self(), ?MODULE, ?LINE | Args])).
 -define(ERR(Format, Args),
-        lager:error("pid=~p m=~p ln=~p class=err " ++ Format,
-                    [self(), ?MODULE, ?LINE | Args])).
+        batchio:format("pid=~p m=~p ln=~p class=err " ++ Format ++ "~n",
+                       [self(), ?MODULE, ?LINE | Args])).
 
 -endif. %logging
