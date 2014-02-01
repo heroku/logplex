a38dfe8bd62a9cebdc666a1279e813c0795daf34
Geoff Cant <nem@erlang.geek.nz>
2014-01-31 16:17:34 -0800

    Rewrite logplex_stats in terms of ectr.

--- src/logplex_stats.erl
+++ src/logplex_stats.erl
@@ -21,19 +21,21 @@
 %% FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 %% OTHER DEALINGS IN THE SOFTWARE.
 -module(logplex_stats).
--behaviour(gen_server).
 
-%% gen_server callbacks
--export([start_link/0, init/1, handle_call/3, handle_cast/2,
-         handle_info/2, terminate/2, code_change/3]).
+%% API
+-export([start_link/0,
+         report/2]).
 
--export([healthcheck/0, incr/1, incr/2, cached/0]).
+-export([healthcheck/0, incr/1, incr/2]).
 
 -include_lib("logplex.hrl").
 
 %% API functions
 start_link() ->
-    gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
+    Interval = timer:seconds(logplex_app:config(stats_reporting_interval, 60)),
+    ectr_srv:start_link(?MODULE,
+                        {?MODULE, report},
+                        Interval).
 
 healthcheck() ->
     redis_helper:healthcheck().
@@ -45,111 +47,13 @@ incr(Key) ->
            #queue_stat{} | list() | atom(), integer()) ->
                   any().
 incr(Key, Incr) when is_integer(Incr) ->
-    try ets:update_counter(?MODULE, Key, Incr)
-    catch error:badarg ->
-            try ets:insert_new(?MODULE, {Key, Incr})
-            catch error:badarg ->
-                    catch ets:update_counter(?MODULE, Key, Incr)
-            end
-    end.
+    ectr_srv:incr(?MODULE, Key, Incr).
 
-cached() ->
-    gen_server:call(?MODULE, cached, 30000).
+unix_ts({Mega, S, _}) ->
+    Mega * 1000000 + S.
 
-%%====================================================================
-%% gen_server callbacks
-%%====================================================================
-
-%%--------------------------------------------------------------------
-%% Function: init(Args) -> {ok, State} |
-%%                         {ok, State, Timeout} |
-%%                         ignore               |
-%%                         {stop, Reason}
-%% Description: Initiates the server
-%% @hidden
-%%--------------------------------------------------------------------
-init([]) ->
-    ets:new(?MODULE, [public, named_table, set, {write_concurrency, true}]),
-    start_timer(),
-    {ok, []}.
-
-%%--------------------------------------------------------------------
-%% Function: %% handle_call(Request, From, State) -> {reply, Reply, State} |
-%%                                      {reply, Reply, State, Timeout} |
-%%                                      {noreply, State} |
-%%                                      {noreply, State, Timeout} |
-%%                                      {stop, Reason, Reply, State} |
-%%                                      {stop, Reason, State}
-%% Description: Handling call messages
-%% @hidden
-%%--------------------------------------------------------------------
-handle_call(cached, _From, Stats) ->
-    {reply, Stats, Stats};
-
-handle_call(_Msg, _From, State) ->
-    {reply, {error, invalid_call}, State}.
-
-%%--------------------------------------------------------------------
-%% Function: handle_cast(Msg, State) -> {noreply, State} |
-%%                                      {noreply, State, Timeout} |
-%%                                      {stop, Reason, State}
-%% Description: Handling cast messages
-%% @hidden
-%%--------------------------------------------------------------------
-handle_cast(_Msg, State) ->
-    {noreply, State}.
-
-%%--------------------------------------------------------------------
-%% Function: handle_info(Info, State) -> {noreply, State} |
-%%                                       {noreply, State, Timeout} |
-%%                                       {stop, Reason, State}
-%% Description: Handling all non call/cast messages
-%% @hidden
-%%--------------------------------------------------------------------
-handle_info({timeout, _TimerRef, flush}, _State) ->
-
-    {Mega, S, _} = os:timestamp(),
-    UnixTS = Mega * 1000000 + S,
-    Stats = ets:tab2list(logplex_stats),
-    [ begin
-          log_stat(UnixTS, K, V),
-          ets:update_counter(?MODULE, K, V * -1)
-      end
-      || {K, V} <- Stats,
-         V =/= 0],
-
-    start_timer(),
-    {noreply, Stats};
-
-handle_info(_Info, State) ->
-    {noreply, State}.
-
-%%--------------------------------------------------------------------
-%% Function: terminate(Reason, State) -> void()
-%% Description: This function is called by a gen_server when it is about to
-%% terminate. It should be the opposite of Module:init/1 and do any necessary
-%% cleaning up. When it returns, the gen_server terminates with Reason.
-%% The return value is ignored.
-%% @hidden
-%%--------------------------------------------------------------------
-terminate(_Reason, _State) ->
-    ok.
-
-%%--------------------------------------------------------------------
-%% Func: code_change(OldVsn, State, Extra) -> {ok, NewState}
-%% Description: Convert process state when code is changed
-%% @hidden
-%%--------------------------------------------------------------------
-code_change(_OldVsn, State, _Extra) ->
-    {ok, State}.
-
-%%--------------------------------------------------------------------
-%%% Internal functions
-%%--------------------------------------------------------------------
-start_timer() ->
-    {_Mega, Secs, Micro} = now(),
-    Time = 60000 - ((Secs rem 60 * 1000) + (Micro div 1000)),
-    erlang:start_timer(Time, ?MODULE, flush).
+report(Now, {K, V}) ->
+    log_stat(unix_ts(Now), K, V).
 
 log_stat(UnixTS, #drain_stat{drain_id=DrainId, channel_id=ChannelId, key=Key}, Val) ->
     io:format("m=logplex_stats ts=~p channel_id=~p drain_id=~p ~p=~p~n",
