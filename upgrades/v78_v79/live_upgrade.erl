ab0163ea68fa13ec45bdb78c9dfbfeb68c14f668
Alex Arnell <alex@heroku.com>
2014-12-19 16:21:56 -0800

    don't fail when process go away

--- upgrades/v78_v79/live_upgrade.erl
+++ upgrades/v78_v79/live_upgrade.erl
@@ -18,16 +18,16 @@ UpgradeNode = fun () ->
     Drains = [ Pid || {Pid, http} <- gproc:lookup_local_properties(drain_type)],
 
     % suspend all the http drains in preparation for a code change
-    [ ok = sys:suspend(Pid, 30000) || Pid <- Drains ],
+    [ ok = sys:suspend(Pid, 60000) || Pid <- Drains, erlang:is_process_alive(Pid) ],
 
     % load the new version of the module
     l(logplex_http_drain),
 
     % perform the state change via code_change
-    [ ok = sys:change_code(Pid, logplex_http_drain, OldVsn, undefined, 30000) || Pid <- Drains],
+    [ ok = sys:change_code(Pid, logplex_http_drain, OldVsn, undefined, 60000) || Pid <- Drins, erlang:is_process_alive(Pid) ],
 
     % resume operation of all the http drains
-    [ ok = sys:resume(Pid, 30000) || Pid <- Drains ],
+    [ ok = sys:resume(Pid, 60000) || Pid <- Drains, erlang:is_process_alive(Pid) ],
 
     io:format(whereis(user), "at=upgrade_end cur_vsn=~p~n", [NextVsn]),
     ok = application:set_env(logplex, git_branch, NextVsn),
