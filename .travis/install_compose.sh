bacb2370092fad59895193a8b3eb8396c7251464
Alex Arnell <alex@heroku.com>
2015-06-22 14:26:28 -0700

    move all travis steps to `.travis.yml`

--- .travis/install_compose.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-curl -L https://github.com/docker/compose/releases/download/1.3.1/docker-compose-`uname -s`-`uname -m` > docker-compose
-chmod +x docker-compose
-sudo mv docker-compose /usr/local/bin
