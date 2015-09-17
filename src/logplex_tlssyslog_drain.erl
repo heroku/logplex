c1833683acb28fbdbde7f25bd7649a63b828802c
Alex Arnell <alex@heroku.com>
2015-09-17 16:04:56 -0700

    reverts some premature refactoring

--- src/logplex_tlssyslog_drain.erl
+++ src/logplex_tlssyslog_drain.erl
@@ -55,7 +55,30 @@
 -export([init/1,  handle_event/3, handle_sync_event/4,
          handle_info/3, terminate/3, code_change/4]).
 
--include("logplex_tlssyslog_drain.hrl").
+-record(state, {drain_id :: logplex_drain:id(),
+                drain_tok :: logplex_drain:token(),
+                channel_id :: logplex_channel:id(),
+                host :: string() | inet:ip_address() | binary(),
+                port :: inet:port_number(),
+                sock = undefined :: 'undefined' | ssl:sslsocket(),
+                %% Buffer for messages while disconnected
+                buf = logplex_msg_buffer:new(default_buf_size()) :: logplex_msg_buffer:buf(),
+                %% Last time we connected or successfully sent data
+                last_good_time :: 'undefined' | erlang:timestamp(),
+                %% TCP failures since last_good_time
+                failures = 0 :: non_neg_integer(),
+                %% Reconnect timer reference
+                reconnect_tref = undefined :: 'undefined' | reference(),
+                %% Send timer reference
+                send_tref = undefined :: 'undefined' | reference(),
+                %% SSL Send connection monitor reference
+                send_mref = undefined :: 'undefined' | reference(),
+                %% Close timer reference
+                close_tref :: reference() | 'undefined',
+                %% Time of last successful connection
+                connect_time :: 'undefined' | erlang:timestamp()
+               }).
+
 
 %% ------------------------------------------------------------------
 %% API Function Definitions
@@ -722,3 +745,4 @@ maybe_shrink(#state{ failures=Tries, buf=Buf }=State) ->
             end
     end.
 
+default_buf_size() -> logplex_app:config(tcp_drain_buffer_size, 1024).
