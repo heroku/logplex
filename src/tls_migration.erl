60fe687b28b32a97b315dc9542c647b5ec7b3414
Sridhar Ratnakumar <srid@heroku.com>
2015-11-09 13:59:36 -0800

    Write CSV output, and act only on TLS errors
    non-TLS errors are logged and ignored.

--- src/tls_migration.erl
+++ src/tls_migration.erl
@@ -1,8 +1,7 @@
 %% TLS migration script (temporary)
 %%
 %% Run from the console as:
-%% > l(tls_migration).
-%% > tls_migration:main(ok).
+%% > l(tls_migration). tls_migration:main(ok).
 
 -module(tls_migration).
 
@@ -13,32 +12,39 @@
 -include("logplex_logging.hrl").
 
 main(_) ->
-    ets:foldl(fun(Drain, Acc) ->
+    ets:foldl(fun(Drain, _Acc) ->
                       case should_migrate_drain(Drain) of
                           true ->
-                              migrate_drain(Drain);
+                              migrate_drain_maybe(Drain);
                           _ ->
                               ok
                       end
               end, notused, drains),
     ok.
 
-migrate_drain(Drain) ->
+migrate_drain_maybe(Drain) ->
     case attempt_connection(Drain) of
         {error, Reason} ->
-            io:format("Drain ~p failed; ~p~n", [Drain, Reason]),
-            toggle_insecure_and_save(Drain);
+            handle_failed_drain(Drain, Reason);
         _ ->
             ok
     end.
+
+handle_failed_drain(Drain, Reason) ->
+    write_csv_row(Drain, Reason),
+    % TODO: only do this on ssl errors (not all kinds of errors)
+    toggle_insecure_and_save(Drain).
 
 attempt_connection(#drain{id=DrainID, channel_id=ChannelID, uri=#ex_uri{authority=#ex_uri_authority{host=Host, port=Port}} = URI}) ->
     io:format("Attempting connection to ~p:~p for drain ~p~n", [Host, Port, DrainID]),
     case logplex_tlssyslog_drain:do_connect(Host, Port, URI, DrainID, ChannelID) of
         {ok, _SslSocket} ->
             ok;
-        {error, Reason} ->
-            {error, Reason}
+        {error, {tls_alert, Alert}=Reason} ->
+            {error, Reason};
+        {error, OtherReason} ->
+            io:format("Non-TLS error ~p~n", [OtherReason]),
+            ok
     end.
 
 toggle_insecure_and_save(Drain) ->
@@ -47,3 +53,9 @@ toggle_insecure_and_save(Drain) ->
 
 should_migrate_drain(#drain{id=_DrainId, type=DrainType, uri=#ex_uri{authority=#ex_uri_authority{host=Host, port=Port}} }) ->
     (DrainType =:= 'tlssyslog') and (Host =:= "logs.papertrailapp.com").
+
+write_csv_row(#drain{id=DrainID, channel_id=ChannelID, uri=DrainURI}=Drain, FailureReason) ->
+    % We just dump CSV rows interleaved with regular logging line, and as such
+    % hackily distinguish them using the CSV prefix (we expect the runner to
+    % filter using `| grep '^CSV'` for processing the CSV data)
+    io:format("CSV ~p, ~p, ~p, ~p~n", [ChannelID, DrainID, DrainURI, FailureReason]).
