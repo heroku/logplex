36ef93103adaec078d5bb888ec99fd2118fcb4a7
Sridhar Ratnakumar <srid@heroku.com>
2015-11-10 14:51:45 -0800

    General refactoring

--- src/tls_migration.erl
+++ src/tls_migration.erl
@@ -8,7 +8,7 @@
 -export([main/1]).
 
 -export([should_migrate_drain/1,
-         migrate_drain_maybe/1,
+         assess_drain_condition/1,
          unflag_all_insecure_drains/0,
          test/0]).
 
@@ -16,55 +16,59 @@
 -include("logplex_drain.hrl").
 -include("logplex_logging.hrl").
 
-main(DryRun)
-  when is_boolean(DryRun) ->
-    MigrateHandlerFun = case DryRun of
+
+%% Mode can be dryrun (for --dry-run). Returns the conditions dict per the list
+%% returned by get_drain_conditions.
+main(Mode) ->
+    Conditions = dict_from_list(get_drain_conditions()),
+    case dict:is_key(needs_migrate, Conditions) of
         true ->
-            fun (_Drain) -> ok end;
+            do_migrate({Mode, dict:fetch(needs_migrate, Conditions)});
         false ->
-            fun mark_drain_as_insecure/1
+            io:format("No drains found worthy of migrating to insecure")
     end,
-    dict_from_list(migrate_all_drains(MigrateHandlerFun)).
+    Conditions.
+
+do_migrate({dryrun, _Conditions}) ->
+    skip;
+do_migrate({_, []}) ->
+    ok;
+do_migrate({_, [{Drain, _Reason} | Rest]}) ->
+    mark_drain_as_insecure(Drain),
+    do_migrate({ok, Rest}).
 
 %% Returns a list of tuple of the form:
-%%    {Action, Drain, Reason}
-%% where Action is one of:
-%%    - migrate :: drain was migrated
-%%    - unhealthy :: drain doesn't even connect in insecure
+%%    {Condition, Drain, Reason}
+%% where Condition is one of:
+%%    - needs_migrate    :: drain was migrated
+%%    - unhealthy        :: drain doesn't even connect in insecure
 %%    - unhealthy_if_tls :: connects in insecure, but fails to verify
-%%    - healthy :: connects fine, no need to migrate
-migrate_all_drains(MigrateHandlerFun) ->
+%%    - healthy          :: connects fine, no need to migrate
+get_drain_conditions() ->
     ets:foldl(fun(Drain, Results) ->
                       case should_migrate_drain(Drain) of
                           true ->
-                              % TODO: sleep before connections to not overload papertrail
-                              case migrate_drain_maybe(Drain) of
-                                  {migrate, {Drain, _Reason}}=Result ->
-                                      MigrateHandlerFun(Drain),
-                                      [Result | Results];
-                                  {_State, {_Drain, _Reason}}=Result ->
-                                      [Result | Results]
-                              end;
+                              [assess_drain_condition(Drain) | Results];
                           _ ->
                               Results
                       end
               end, [], drains).
 
-migrate_drain_maybe(Drain) ->
+assess_drain_condition(Drain) ->
     case attempt_connection({insecure, Drain}) of
         {error, Reason} ->
             % Drain never connects in the first place.
             {unhealthy, {Drain, Reason} };
         ok ->
             % Drains works, so let's try migrating it.
-            migrate_drain(Drain)
+            do_assess_drain_condition(Drain)
     end.
 
-migrate_drain(Drain) ->
+do_assess_drain_condition(Drain) ->
     case attempt_connection({default, Drain}) of
         {error, {tls_alert, _Alert}=Reason} ->
             % Verification failed. Make this insecure.
-            {migrate, {Drain, Reason} };
+            {needs_migrate, {Drain, Reason} };
         {error, Reason} ->
             {unhealthy_if_tls, {Drain, Reason} };
         ok ->
@@ -78,6 +82,7 @@ attempt_connection({insecure, #drain{uri=URI}=Drain}) ->
                logplex_drain:uri_to_binary(URI))),
     attempt_connection({default, Drain#drain{uri=NewURI}});
 attempt_connection({default, #drain{id=DrainID, channel_id=ChannelID, uri=#ex_uri{authority=#ex_uri_authority{host=Host, port=Port}} = URI}}) ->
+    %% TODO: sleep before connections to not overload papertrail
     io:format("Attempting connection to ~p:~p for drain ~p~n", [Host, Port, DrainID]),
     case logplex_tlssyslog_drain:do_connect(Host, Port, URI, DrainID, ChannelID) of
         {ok, _SslSocket} ->
@@ -87,8 +92,6 @@ attempt_connection({default, #drain{id=DrainID, channel_id=ChannelID, uri=#ex_ur
             io:format("TLS error ~p~n", [Reason]),
             {error, Reason};
         {error, OtherReason} ->
-            % TODO: what to do with this? timeouts should be retried.
-            %  maybe return 'other' failed drains in the accumulator
             io:format("Non-TLS error ~p~n", [OtherReason]),
             {error, OtherReason}
     end.
@@ -104,10 +107,10 @@ should_migrate_drain(#drain{}) ->
     false.
 
 mark_drain_as_insecure(#drain{uri=URI}=Drain) ->
-    update_drain_uri(Drain, new_uri_with_insecure(logplex_drain:uri_to_binary(URI))).
+    update_drain_uri_in_redis(Drain, new_uri_with_insecure(logplex_drain:uri_to_binary(URI))).
 
 unmark_drain_as_insecure(#drain{uri=URI}=Drain) ->
-    update_drain_uri(Drain, new_uri_without_insecure(logplex_drain:uri_to_binary(URI))).
+    update_drain_uri_in_redis(Drain, new_uri_without_insecure(logplex_drain:uri_to_binary(URI))).
 
 new_uri_with_insecure(URI) ->
     erlang:iolist_to_binary([URI, <<"#insecure">>]).
@@ -119,7 +122,7 @@ new_uri_without_insecure(URI) ->
                  "",
                  [{return,list}])).
 
-update_drain_uri(#drain{id=DrainID, channel_id=ChannelID, token=Token}, NewURI) ->
+update_drain_uri_in_redis(#drain{id=DrainID, channel_id=ChannelID, token=Token}, NewURI) ->
     io:format("Changing drain ~p URI to ~p~n", [DrainID, NewURI]),
     case redis_helper:create_url_drain(DrainID, ChannelID, Token, NewURI) of
         ok ->
@@ -128,6 +131,7 @@ update_drain_uri(#drain{id=DrainID, channel_id=ChannelID, token=Token}, NewURI) 
             io:format("REDIS ERROR updating drain ~p: ~p~n", [DrainID, Err])
     end.
 
+%% Convert a list of {Key, Value} to a dict of {Key => [Values...]}
 dict_from_list(Items) when is_list(Items) ->
     lists:foldl(fun({Key, Value}, Dict) ->
                         dict:append(Key, Value, Dict)
@@ -135,7 +139,8 @@ dict_from_list(Items) when is_list(Items) ->
                 dict:new(),
                 Items).
 
-% ~~ Code that follows below are for testing only ~~
+
+&% ~~ Code that follows below are for testing only ~~
 
 % To undo the effects of running this script.
 unflag_all_insecure_drains() ->
@@ -145,4 +150,4 @@ unflag_all_insecure_drains() ->
 
 test() ->
     unflag_all_insecure_drains(),
-    main(true).
+    main(ok).
