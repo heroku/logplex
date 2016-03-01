075fe9fd715c746c5b36d34fd89b8f2265d756d4
Sridhar Ratnakumar <srid@heroku.com>
2016-03-01 10:35:07 -0800

    Log using appropriate levels

--- src/logplex_logging.hrl
+++ src/logplex_logging.hrl
@@ -12,12 +12,12 @@
           io_lib:format("pid=~p m=~p ln=~p class=info " ++ Format ++ "~n",
                         [self(), ?MODULE, ?LINE | Args]))).
 -define(WARN(Format, Args),
-        syslog_lib:notice(
+        syslog_lib:warning(
           syslog_tab,
           io_lib:format("pid=~p m=~p ln=~p class=warn " ++ Format ++ "~n",
                         [self(), ?MODULE, ?LINE | Args]))).
 -define(ERR(Format, Args),
-        syslog_lib:notice(
+        syslog_lib:error(
           syslog_tab,
           io_lib:format("pid=~p m=~p ln=~p class=err " ++ Format ++ "~n",
                         [self(), ?MODULE, ?LINE | Args]))).
