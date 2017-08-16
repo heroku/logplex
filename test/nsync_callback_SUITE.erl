d2ccd8fc3a99cf36c030f5178fe80415578bb1f2
Kevin McDermott <kevin@heroku.com>
2017-08-16 14:06:29 +0100

    Catch new v3 tokens and silently drop them.
    This adds handling to nsync_callback:convert_to_integer to return either {ok,
    Converted} or {error, bad_token} when appropriate.
    
    It updates the two callers of convert_to_integer to check the result, and
    silently drop if we can't process the token.

---
+++ test/nsync_callback_SUITE.erl
@@ -0,0 +1,56 @@
+-module(nsync_callback_SUITE).
+-include_lib("common_test/include/ct.hrl").
+-include_lib("eunit/include/eunit.hrl").
+
+-compile(export_all).
+
+all() ->
+    [run_eunit,
+    find_token_with_missing_channel,
+    find_token_with_channel,
+    find_token_with_invalid_channel,
+    create_token_with_invalid_channel,
+    create_or_update_drain_with_missing_channel,
+    create_or_update_drain_with_invalid_channel].
+
+run_eunit(_Config) ->
+    ok = eunit:test(nsync_callback).
+
+find_token_with_missing_channel(Config) ->
+    Result = nsync_callback:find_token("12345", dict:new()),
+    ?assertEqual({error, missing_channel}, Result),
+    Config.
+
+find_token_with_channel(Config) ->
+    Dict1 = dict:store(<<"ch">>, <<"12345">>, dict:new()),
+    Dict2 = dict:store(<<"name">>, <<"testing">>, Dict1),
+    Result = nsync_callback:find_token(<<"testing">>, Dict2),
+    ?assertEqual({ok,{token,<<"testing">>,12345,<<"testing">>}}, Result),
+    Config.
+
+find_token_with_invalid_channel(Config) ->
+    Dict1 = dict:store(<<"ch">>, <<"abcdefg">>, dict:new()),
+    Dict2 = dict:store(<<"name">>, <<"testing">>, Dict1),
+    Result = nsync_callback:find_token(<<"testing">>, Dict2),
+    ?assertEqual({error, bad_token}, Result),
+    Config.
+
+create_token_with_invalid_channel(Config) ->
+    Dict1 = dict:store(<<"ch">>, <<"abcdefg">>, dict:new()),
+    Dict2 = dict:store(<<"name">>, <<"testing">>, Dict1),
+    Result = nsync_callback:create_token(<<"testing">>, Dict2),
+    ?assertEqual(ok, Result),
+    Config.
+
+create_or_update_drain_with_missing_channel(Config) ->
+    Result = nsync_callback:create_or_update_drain("12345", dict:new()),
+    ?assertEqual(ok, Result),
+    Config.
+
+create_or_update_drain_with_invalid_channel(Config) ->
+    Dict1 = dict:store(<<"ch">>, <<"abcdefg">>, dict:new()),
+    Dict2 = dict:store(<<"name">>, <<"testing">>, Dict1),
+    Dict3 = dict:store(<<"token">>, <<"testing">>, Dict2),
+    Result = nsync_callback:create_or_update_drain(<<"testing">>, Dict3),
+    ?assertEqual(ok, Result),
+    Config.
